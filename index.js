d9cb21048b69c8852e3dd5670599ff211b3f43d3
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2024-10-29 12:06:19 +0000

    Add functions to handle the basic math operators

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+function add(...args) {
+   let additionOutput = args.reduce((sum, nextNumber) => {
+      return sum + nextNumber;
+   });
+
+   return additionOutput;
+}
+
+function subtract(...args) {
+   let subtractionOutput = args.reduce((remainder, nextNumber) => {
+      return remainder - nextNumber;
+   });
+
+   return subtractionOutput;
+}
+
+function multiply(...args) {
+   let multiplicationOutput = args.reduce((product, nextNumber) => {
+      return product * nextNumber;
+   });
+
+   return multiplicationOutput;
+}
+
+function divide(...args) {
+   let divisionOutput = args.reduce((quotient, nextNumber) => {
+      return quotient / nextNumber;
+   });
+
+   return divisionOutput;
+}
+
 function adjustHeight() {
    let parentDivInBody = document.querySelector(".body-inner");
    let vh = window.innerHeight * 0.01;
